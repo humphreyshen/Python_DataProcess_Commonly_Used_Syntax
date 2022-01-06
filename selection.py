f607ac7b0fe19c3c61f67cc47ec2ef99e86cab3b
humphreyshen <humphreyshen1003@gmail.com>
2022-01-06 18:11:40 +0800

    main

---
+++ selection.py
@@ -0,0 +1,14 @@
+# Selected by column names
+Df1=Df.loc[:,['Column A','Column B']
+           
+# Selected by index      
+Df1=Df.iloc[:,3]
+
+# Selected by index and adding range on raws
+Df1=DF.iloc[1:3,:]
+
+# Selected by column condition
+Df.loc[:,[zh_name]=='XXX']
+
+# Selected by column condition
+Df[Df.zh_name=='XXX']
