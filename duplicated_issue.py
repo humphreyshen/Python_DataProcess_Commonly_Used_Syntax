1f059ff87d25318140e1a401c916cd8537747df1
humphreyshen <humphreyshen1003@gmail.com>
2022-01-06 18:25:32 +0800

    main

--- duplicated_issue.py
+++ duplicated_issue.py
@@ -1,3 +1,7 @@
-# Droppping duplicated data from ceratin column within a DataFrame
+# Droppping duplicated data from certain column within a DataFrame
 Df1=Df.dropduplicated(subset=['Coumn A'])
 
+# Finding duplicated data from certain column within a DataFrame
+Df1=Df.duplicated(subset=['Column A'], Keep=False) # Keep all of the duplicated data
+Df1=Df.duplicated(subset=['Column A'], Keep=first) # Keep the first data of the duplicated data set
+Df1=Df.duplicated(subset=['Column A'], Keep=last) # Keep the last data of the duplicated data set
